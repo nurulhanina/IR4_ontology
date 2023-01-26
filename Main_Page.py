22b407664e6a6ff193a707e7be0a537a79547045
nurulhanina <wid190044@siswa.um.edu.my>
2023-01-26 17:08:32 +0800

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -2,6 +2,6 @@ import streamlit as st
 from PIL import Image
 
 st.title("SISTEM ISTILAH DWIBAHASA")
-image=Image.open('Resources\Header.png')
+image=Image.open('https://github.com/nurulhanina/IR4_ontology/blob/92998ea96c6ccef10a38eec512fa8c1a4e1c3c0b/Resources/Header.png')
 st.image(image)
 
