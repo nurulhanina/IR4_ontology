710039b1c14683744b059359a4ef10e209d9194d
nurulhanina <wid190044@siswa.um.edu.my>
2023-01-26 19:43:19 +0800

    Update 01_Search.py

--- pages/01_Search.py
+++ pages/01_Search.py
@@ -8,18 +8,4 @@ import gspread
 st.title("Sistem Istilah Dwibahasa")
 st.markdown("Search Query")
 
-json_auth='gs_credential.json'
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/spreadsheets']
-creds = ServiceAccountCredentials.from_json_keyfile_name('json_auth', scope=scope)
-client = gspread.authorize(creds)
-sheet = client.open('ir4_database')
-
-# get the first sheet of the Spreadsheet
-sheet_instance = sheet.get_worksheet(0)
-records_data = sheet_instance.get_all_records()
-records_df = pd.DataFrame.from_dict(records_data)
-# view the top records
-#records_df.head()
-st.markdown(sheet_instance.cell(col=3,row=2))
-
 
